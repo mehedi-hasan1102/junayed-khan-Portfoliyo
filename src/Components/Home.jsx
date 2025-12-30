b84459f68859b6c7973c2096e749ede4b76da748
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2025-12-30 22:49:29 +0600

    changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,7 +10,6 @@ import Footer from './Footer';
 import PortfolioSection from '../Components/PortfolioSection';
 import Services from './Services';
 import Contact from './Contact';
-import HomeBlogs from './Blogs/HomeBlogs';
 
 export default function ScrollLinked() {
     const { scrollYProgress } = useScroll();
@@ -46,7 +45,6 @@ function Content() {
             <Hero />
             <Services />
             <PortfolioSection />
-            <HomeBlogs />
             <Contact />
         </article>
     );
